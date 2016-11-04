d3f99ae67f85220564521ed583aac223f22b9551
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-04 13:28:00 +0100

    Nieuwe zip naam

--- eindopdracht/make.sh
+++ eindopdracht/make.sh
@@ -1,8 +1,8 @@
-if test -e s1094220.zip; then
-    rm s1094220.zip
+if test -e s1094220-IPMEDT2-twitter-feed.zip; then
+    rm s1094220-IPMEDT2-twitter-feed.zip
     echo "De oude zip is verwijderd!"
 fi
 
-if zip -r -q s1094220.zip .; then
+if zip -r -q s1094220-IPMEDT2-twitter-feed.zip .; then
     echo "Het zippen is gelukt!"
 fi
